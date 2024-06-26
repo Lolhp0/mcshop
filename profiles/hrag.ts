62650a7e744bbe40874bfb3b18a039597af15629
Hagop Hovaguimian <hagop.microchip@gmail.com>
2024-06-26 23:30:32 +0400

    Update: Description of changes made

--- profiles/hrag.ts
+++ profiles/hrag.ts
@@ -5,16 +5,16 @@ export const hragY = {
         banner: "",
     },
     shop: [
-        // {
-        //     item_name: "Diamonds",
-        //     item_description: "My Diamonds",
-        //     item_type: "diamond",
-        //     item_price: "64",
-        //     item_quantity: 28,
-        //     item_information: {
-        //         item_enchants: [],
-        //         item_used: false
-        //     },
-        // },
+        {
+            item_name: "",
+            item_description: "",
+            item_type: "",
+            item_price: "",
+            item_quantity: 0,
+            item_information: {
+                item_enchants: [],
+                item_used: false
+            },
+        },
     ],
 }
